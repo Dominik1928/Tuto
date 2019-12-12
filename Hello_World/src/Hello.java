59a694d467e0c43d5f9485af1d23434ebe135d32
Dominik Jodenschwager <do.jodenschwager@gmail.com>
2019-12-12 14:23:15 +0100

    variables.class hinzugefügt und hello.class verändert

--- Hello_World/src/Hello.java
+++ Hello_World/src/Hello.java
@@ -1,3 +1,12 @@
-public class Hello {
-    
+public class Hello extends Variables {
+    public static void main(String[] args){
+
+        Variables variables = new Variables();
+
+
+        System.out.println("Hello World!");
+        System.out.println("\n");
+        System.out.println(variables.getaString());
+
+    }
 }
---
+++ Hello_World/src/Variables.java
@@ -0,0 +1,13 @@
+
+
+public class Variables {
+    int anInt = 1;
+    double aDouble = 2.5;
+    String aString = "String 1";
+    boolean aBoolean = true;
+
+    public String getaString(){
+
+        return this.aString;
+    }
+}
